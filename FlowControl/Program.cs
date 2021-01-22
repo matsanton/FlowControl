68c8730b70c29b8b05667df9da413855a1386572
Mats Antonsson <matan848@hotmail.com>
2021-01-22 15:58:45 +0100

    Add project files.

---
+++ FlowControl/Program.cs
@@ -0,0 +1,27 @@
+﻿// Övning 2
+using System;
+
+
+namespace FlowControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                DisplayMainMenu();
+
+
+            }
+
+        }
+
+        private static void DisplayMainMenu()
+        {
+            Console.WriteLine("*** Huvudmeny ***");
+            Console.WriteLine("0. Avsluta");
+            Console.WriteLine("1.");
+        }
+    }
+}
